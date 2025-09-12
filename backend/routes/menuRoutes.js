5151e60c853148a6637bbfc73644b5d8c3d96cfc
Kkkaique <kaiquepotato@gmail.com>
2025-09-12 17:55:50 -0300

    Atualiza menuRoutes com rotas corretas do menu

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -5,7 +5,7 @@ const menuController = require('../controllers/menuController');
 // Rotas de autenticação
 
 router.get('/', menuController.abrirMenu);
-// router.post('/inicio', menuController.inicio);
+router.post('/inicio', menuController.inicio);
 router.post('/logout', menuController.logout);
 
 module.exports = router;
