6ff82fea69ea88c02bb1f3b989c1111e2ba7061d
gabriele dos santos cardoso <gabrielecardoso@alunos.utfpr.edu.br>
2025-10-02 21:25:18 -0300

    crudproduto

--- backend/server.js
+++ backend/server.js
@@ -76,14 +76,14 @@ app.use((err, req, res, next) => {
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/', menuRoutes);
 
-const pessoaRoutes = require('./routes/pessoaRoutes');
-app.use('/pessoa', pessoaRoutes);
+//const pessoaRoutes = require('./routes/pessoaRoutes');
+//app.use('/pessoa', pessoaRoutes);
 
 //const questaoRoutes = require('./routes/questaoRoutes');
 //app.use('/questao', questaoRoutes);
 
-//const professorRoutes = require('./routes/professorRoutes');
-//app.use('/professor', professorRoutes);
+const produtoRoutes = require('./routes/produtoRoutes');
+app.use('/produto', produtoRoutes);
 
 //const avaliadorRoutes = require('./routes/avaliadorRoutes');
 //app.use('/avaliador', avaliadorRoutes);
